790743db5920c7de02a321a9ed7aa16b55cfd398
Bot Wea ID <91173360+botwea-ID@users.noreply.github.com>
2021-09-22 22:41:15 +0700

    Update db.js

--- models/db.js
+++ models/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const MONGO_URI = process.env.MONGO_URL || 'mongodb://localhost:27017/url'; 
+const MONGO_URI = 'mongodb+srv://Franky404:frangki12345@cluster0.u6x4y.mongodb.net/myFirstDatabase?retryWrites=true&w=majority' || 'mongodb://localhost:27017/url'; 
 const connect = () => {
   mongoose.connect(MONGO_URI, {useNewUrlParser : true, useUnifiedTopology : true})
   .then(() => console.log('DB connected'))
